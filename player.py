eead32a31fae01f1c3e9ce4838c1d556622c1e27
samasriv99 <samarthsriv2599@gmail.com>
2026-05-14 21:30:44 +0000

    Added the update method for user control

---
+++ player.py
@@ -0,0 +1,30 @@
+import circleshape
+from circleshape import CircleShape
+import constants
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+import pygame
+
+class Player(CircleShape):
+    def __init__(self, x,y):
+        super().__init__(x,y,PLAYER_RADIUS)
+        self.rotation = 0
+    
+    # in the Player class
+    def rotate(self, dt):
+        return PLAYER_TURN_SPEED*dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+            
+               
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
