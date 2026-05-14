2361c8a18a0c88b74175c21b951ce58998a8e257
samasriv99 <samarthsriv2599@gmail.com>
2026-05-14 12:19:39 +0000

    Setting up the GUI and checking the dimensions of the screen

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+import constants
+import logger
+from logger import log_state
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    print(f"Starting Asteroids with pygame version : {pygame.version.ver}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    while True:
+        logger.log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
